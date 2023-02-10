8d1609e3a60720386720be5183e7a77f4654939f
Martins Paberzs <martins.paberzs@blockvis.com>
2023-02-10 14:29:26 +0200

    change xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -13,24 +13,36 @@ import XMonad.Layout.LayoutCombinators    -- use the one from LayoutCombinators 
 import XMonad.Hooks.DynamicLog (dynamicLogWithPP, wrap, xmobarPP, xmobarColor, shorten, PP(..))
 import System.IO (hPutStrLn)
 import qualified XMonad.StackSet as W
+import XMonad.Hooks.FadeInactive
+import XMonad.Layout.Spacing
 
 
 startup = do
     setWMName "LG3D"
     spawnOnce "picom &"
-    spawn "nitrogen --restore &"
+    spawn "nitrogen --set-tiled ~/Downloads/tilel.png &"
+    -- spawnOnce "/usr/bin/gnome-keyring-daemon --start --foreground --components=secrets && nm-applet --indicator &"
     spawnOnce "nm-applet --indicator &"
-    spawnOnce "trayer --widthtype pixel --width 80 --align right --edge top --height 22 --tint 0x00000000 --transparent true --alpha 1"
+    spawnOnce "trayer --monitor 1 --widthtype pixel --width 80 --align right --edge top --height 22 --tint 0x00000000 --transparent true --alpha 1 &"
+
+myFadeInactiveLogHook :: X ()
+myFadeInactiveLogHook = fadeInactiveLogHook fadeAmount
+  where fadeAmount = 0.95
 
 main = do
-  xmproc <- spawnPipe "xmobar ~/.config/xmobar/xmobarrc1"
-  xmproc <- spawnPipe "xmobar ~/.config/xmobar/xmobarrc2"
+  -- when in work with primary horizontal and left of it vertical secondary
+  -- xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobarrc2"
+  -- xmproc <- spawnPipe "xmobar -x 1 ~/.config/xmobar/xmobarrc1"
+  -- when only laptop screen
+  xmproc <- spawnPipe "xmobar -x 0 ~/.config/xmobar/xmobarrc_mobile"
   xmonad $ desktopConfig
     { terminal    = "alacritty"
     , modMask     = mod4Mask
-    , borderWidth = 2
+    , borderWidth = 1
+    , focusedBorderColor = "#FD7F39" -- orange
     , startupHook = startup
     , layoutHook  = layout
+    , logHook     = myFadeInactiveLogHook
 --    , logHook     = dynamicLogWithPP $ xmobarPP {
 --    }
     }
@@ -54,11 +66,15 @@ myKeys = [
      , ((0, xF86XK_MonBrightnessUp),   spawn "brightnessctl set +5%")
      , ((0, xF86XK_MonBrightnessDown), spawn "brightnessctl set 5%-")
 --     , ((mod4Mask, xK_F12), spawn "loginctl lock-session")
-     , ((mod4Mask, xK_F12), spawn "i3lock --image ~/Downloads/leopard-3440-2560.png")
-     , ((mod4Mask .|. shiftMask, xK_F12), spawn "i3lock --image ~/Downloads/leopard-3440-2560.png && systemctl suspend")
+     -- , ((mod4Mask, xK_F12), spawn "xss-lock -- i3lock --no-fork")
+     , ((mod4Mask, xK_F12), spawn "i3lock --nofork -t -i ~/Downloads/tileb.png")
+--     , ((mod4Mask .|. shiftMask, xK_F12), spawn "xss-lock --transfer-sleep-lock -- i3lock --no-fork")
+     , ((mod4Mask .|. shiftMask, xK_F12), spawn "i3lock -t -i ~/Downloads/tileb.png && systemctl suspend")
      , ((mod4Mask, xK_F10), prompt "nmcli c u martins_parsiq_staging" greenXPConfig)
      , ((mod4Mask .|. shiftMask, xK_F10), prompt "nmcli c d martins_parsiq_staging" greenXPConfig)
-     , ((mod4Mask, xK_r), spawn "dmenu_run -fn 'Inconsolata 12'")
+     , ((mod4Mask, xK_F11), prompt "nmcli c u martins_parsiq_prod" greenXPConfig)
+     , ((mod4Mask .|. shiftMask, xK_F11), prompt "nmcli c d martins_parsiq_prod" greenXPConfig)
+     , ((mod4Mask, xK_d), spawn "dmenu_run -m 0 -fn 'Inconsolata 12'")
      , ((mod4Mask, xK_p), spawn "autorandr --change")
      , ((mod4Mask, xK_n), spawn "xterm -e 'nmtui-connect'")
      , ((mod4Mask, xK_Print), spawn "spectacle -rc")
@@ -73,7 +89,7 @@ myKeys = [
 layout = avoidStruts(tiled ||| Mirror tiled ||| noBorders Full)
   where
      -- default tiling algorithm partitions the screen into two panes
-     tiled   = Tall nmaster delta ratio
+     tiled   = smartSpacing 10 $ Tall nmaster delta ratio
      
      -- The default number of windows in the master pane
      nmaster = 1
