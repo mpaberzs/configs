43414c497d7cbde7607136da25aec87f554211c8
Martins Paberzs <martins.paberzs@eee.do>
2024-03-17 18:07:51 +0200

    chore: scripts

--- scripts/cabal-xmonad-xmobar-install.sh
+++ scripts/cabal-xmonad-xmobar-install.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
-# --ghc-options=-dynamic --ghc
 rm -rf ~/.cabal
 cabal update
-cabal install --lib assoc-list xmonad xmonad-utils xmonad-contrib xmonad-extras X11 --allow-newer --overwrite-policy=always
-cabal install xmonad --overwrite-policy=always --allow-newer
-cabal install xmobar --flags="all_extensions" --overwrite-policy=always --allow-newer
+cabal install --ghc-options=-dynamic --ghc --lib assoc-list xmonad xmonad-utils xmonad-contrib xmonad-extras X11 --allow-newer --overwrite-policy=always
+cabal install xmonad --ghc-options=-dynamic --ghc --overwrite-policy=always --allow-newer
+cabal install xmobar --ghc-options=-dynamic --ghc --flags="all_extensions" --overwrite-policy=always --allow-newer
