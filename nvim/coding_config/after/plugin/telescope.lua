3306d1117e170ddea3d78e68e4e6d1e1593ef8f7
Martins Paberzs <martins.paberzs@eee.do>
2024-08-08 17:05:05 +0300

    feat: improve telescope

--- nvim/coding_config/after/plugin/telescope.lua
+++ nvim/coding_config/after/plugin/telescope.lua
@@ -1,7 +1,16 @@
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+-- vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fg', builtin.git_files, {})
+vim.keymap.set('n', '<leader>ff', function() builtin.git_files({ show_untracked = true, use_git_root = false }); end, {})
+
+vim.keymap.set('n', '<leader>fdf', function() 
+  builtin.git_files({ show_untracked = true, use_git_root = false, cwd = vim.fn.input("files in dir: ") }); 
+end, {})
+
+vim.keymap.set('n', '<leader>fdw', function() 
+  builtin.live_grep({ cwd = vim.fn.input("grep in dir: ") }); 
+end, {})
+
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
@@ -9,3 +18,28 @@ vim.keymap.set('n', 'gD', builtin.lsp_type_definitions, {})
 vim.keymap.set('n', 'gr', builtin.lsp_implementations, {})
 vim.keymap.set('n', '<leader>q', builtin.diagnostics, {})
 --vim.keymap.set('n', '<leader>gt', builtin.git_status, {})
+
+require('telescope').setup{
+  defaults = {
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+    }
+  },
+  pickers = {
+    find_files = {
+      additional_args = {
+        "--smart-case",
+      }
+    },
+    git_files = {
+      additional_args = {
+        "--smart-case",
+      }
+    },
+  }
+}
